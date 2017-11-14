e8b03a1a1fd49589c02efba9c616dfdc4588aa01
alaniz <alanizbtc@gmail.com>
2017-11-14 21:00:39 +0000

    Changed PoW algorithm to Scrypt

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -9,6 +9,7 @@
 #include "tinyformat.h"
 #include "utilstrencodings.h"
 #include "crypto/common.h"
+#include "crypto/scrypt.h"
 #include "hashblock.h"
 
 uint256 CBlockHeader::GetHash() const
@@ -21,7 +22,9 @@ uint256 CBlockHeader::GetHash() const
 
 uint256 CBlockHeader::GetPoWHash() const
 {
- return Hash9(BEGIN(nVersion), END(nNonce));
+    uint256 thash;
+    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    return thash;
 }
 
 std::string CBlock::ToString() const
