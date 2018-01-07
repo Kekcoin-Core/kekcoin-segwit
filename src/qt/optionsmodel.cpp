8a0023ee4468e731287fe88d8df71159e4582863
alaniz <alaniz@linuxmail.org>
2018-01-07 16:25:13 +0000

    Change block explorer to explorer.kekcoin.co

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -72,8 +72,8 @@ void OptionsModel::Init(bool resetSettings)
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "https://chainz.cryptoid.info/nav/tx.dws?%s.htm");
-    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "https://chainz.cryptoid.info/nav/tx.dws?%s.htm").toString();
+        settings.setValue("strThirdPartyTxUrls", "http://explorer.kekcoin.co/tx/%s");
+    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "http://explorer.kekcoin.co/tx/%s").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
         settings.setValue("fCoinControlFeatures", false);
