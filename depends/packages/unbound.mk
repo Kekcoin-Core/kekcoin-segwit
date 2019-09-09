ef945a886c4ee6c69c9abd51355d885084203aed
bitmammoth <bitmammoth@gmail.com>
2019-09-09 11:50:07 -0700

    updated qt depends

--- depends/packages/unbound.mk
+++ depends/packages/unbound.mk
@@ -1,19 +1,17 @@
 package=unbound
-$(package)_version=1.6.7
+$(package)_version=1.9.0
 $(package)_download_path=http://unbound.net/downloads/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=4e7bd43d827004c6d51bef73adf941798e4588bdb40de5e79d89034d69751c9f
-$(package)_dependencies=openssl
-$(package)_patches=fix_pkg_config.patch
-$(package)_deptrack=1
-$(package)_cppflags=-I/usr/include
+$(package)_sha256_hash=415af94b8392bc6b2c52e44ac8f17935cc6ddf2cc81edfb47c5be4ad205ab917
+$(package)_dependencies=openssl expat
+$(package)_patches=getentropy.patch
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/fix_pkg_config.patch
+  patch -p1 < $($(package)_patch_dir)/getentropy.patch
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--with-ssl=$(host_prefix) --disable-gost --disable-ecdsa
+  $(package)_config_opts=--with-ssl=$(host_prefix) --with-libexpat=$(host_prefix) --disable-gost --disable-ecdsa --disable-shared --with-pic
 endef
 
 define $(package)_config_cmds
@@ -25,7 +23,7 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-all
 endef
 
 define $(package)_postprocess_cmds
